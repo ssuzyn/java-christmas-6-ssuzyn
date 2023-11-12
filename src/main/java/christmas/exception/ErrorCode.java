e5a89bda27dff7525df66408f03e29e3b614d46c
ssuzyn <asj7090@gmail.com>
2023-11-12 16:22:40 +0900

    :art: [style] : 에러 코드 변수명 변경

--- src/main/java/christmas/exception/ErrorCode.java
+++ src/main/java/christmas/exception/ErrorCode.java
@@ -5,7 +5,7 @@ public enum ErrorCode {
     PREFIX("[ERROR] "),
     NOT_INTEGER("입력한 값이 숫자가 아닙니다. 다시 입력해 주세요."),
     INVALID_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    MENU_NOT_FOUND("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    INVALID_MENU("유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
     private final String message;
 
