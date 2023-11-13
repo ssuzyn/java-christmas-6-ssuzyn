a7bbaa5e42621b86c1912fa4df1fac18998eba88
ssuzyn <asj7090@gmail.com>
2023-11-14 00:37:00 +0900

    :white_check_mark: [test] : 주문메뉴 가격 계산 기능 테스트 코드 작성

--- src/test/java/christmas/domain/CustomerTest.java
+++ src/test/java/christmas/domain/CustomerTest.java
@@ -11,19 +11,32 @@ import org.junit.jupiter.api.Test;
 
 public class CustomerTest {
 
-    private Map<Menu, Integer> orderMenu = new HashMap<>();;
+    private Map<Menu, Integer> orderMenu = new HashMap<>();
     private Customer customer;
+
+    @Test
+    @DisplayName("주문메뉴 가격 계산 기능 테스트")
+    void calculateTest(){
+        initCustomer();
+        long price = customer.calculatePrice();
+
+        assertThat(price).isEqualTo(121000);
+    }
 
     @Test
     @DisplayName("메인 메뉴 갯수 카운트 테스트")
     void MainMenuCount(){
+        initCustomer();
+        int count = customer.countMenuType(MenuType.MAIN);
+
+        assertThat(count).isEqualTo(2);
+    }
+
+    void initCustomer(){
         orderMenu.put(Menu.T_BONE_STEAK, 2);
         orderMenu.put(Menu.ZERO_COLA, 2);
         orderMenu.put(Menu.ICE_CREAM, 1);
 
         customer = new Customer(25, orderMenu);
-        int count = customer.countMenuType(MenuType.MAIN);
-
-        assertThat(count).isEqualTo(2);
     }
 }
